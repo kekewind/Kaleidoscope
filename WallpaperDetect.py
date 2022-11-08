e3a74587db84524c4be9dd34d3eaced0a945470b
v000666 <3190103335@zju.edu.cn>
2022-11-08 21:54:25 +0800

    latest -WYJ

--- WallpaperDetect.py
+++ WallpaperDetect.py
@@ -14,14 +14,15 @@ def main():
     fun()
     while True:
         # 复制网址并关闭网页
-        pyautogui.hotkey('alt','tab')
         pyautogui.click(1075,72)
         time.sleep(0.2)
         pyautogui.hotkey('ctrl','c')
         time.sleep(0.2)
         pyautogui.hotkey('ctrl','w')
-        readytodownload.add(pyperclip.paste())
-        break
+        time.sleep(0.2)
+        readytodownload.add({''
+                             :[pyperclip.paste()]})
+pyautogui.hotkey('alt','tab')
 
 
 
@@ -30,4 +31,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-    page=MyUtils.Chrome(silent=True)
--- WallpaperDownload.py
+++ WallpaperDownload.py
@@ -1,12 +1,24 @@
 import MyUtils
 import WallpaperUtils
+import pyperclip
+import WallpaperUtils
+readytodownload=WallpaperUtils.readytodownload
 
-def fun():
 
-    page.get(pyperclip.paste())
-    readytodownload.add(page.element('//*[@id="wallpaper"]').get_attribute('src'))
 
 # 后台下载到图片/未分类
+page = MyUtils.Chrome(silent=True)
+def fun():
+    path=f'C:/{MyUtils.user}/Pictures/WallPaper/static/未分类'
+#     开始下载
+    url=MyUtils.value(readytodownload.get())[0]
+    while not url==None:
+        page.get(url)
+        url=(page.element('//*[@id="wallpaper"]').get_attribute('src'))
+        MyUtils.pagedownload(url,f'{path}/{MyUtils.tail("/")}',t=3)
+        url=readytodownload.get()
+
+
 def main():
     fun()
 
