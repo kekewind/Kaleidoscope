7cf6a1c1f72e40c411b26d16332d133d7fa16346
v000666 <3190103335@zju.edu.cn>
2022-11-08 20:23:24 +0800

    latest -WYJ

--- ALuQi.py
+++ ALuQi.py
@@ -8,7 +8,6 @@ for file in MyUtils.listfile(['./', './璐琪/']):
             year, month, day = filename[5:9], filename[9:11], filename[11:13]
         if 'Scre' in filename:
             year, month, day = filename[15:19], filename[20:22], filename[23:25]
-        # MyUtils.log(f"move({file}, './璐琪/{year}-{month}-{day}')")
         targetdir=f'./璐琪/{year}-{month}-{day}'
         if not MyUtils.isfile(targetdir + filename):
             MyUtils.move(file, targetdir)
@@ -18,3 +17,4 @@ f = MyUtils.rjson(MyUtils.root + '璐琪/record')
 for dir in MyUtils.listdir(['./璐琪/']):
     for file in MyUtils.listfile(dir):
         f.add({MyUtils.filename(file): MyUtils.diskname})
+
