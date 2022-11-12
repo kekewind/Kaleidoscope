21fd46d923d9f6cb35029d9929fc4387da51b8da
v000666 <3190103335@zju.edu.cn>
2022-11-12 21:30:38 +0800

    latest -WYJ

--- TEST/basic.py
+++ TEST/basic.py
@@ -1,22 +1,8 @@
-import concurrent.futures
-import os
-import sys
-
-import requests
-import time
-from collections.abc import Iterable
-import multi
-import cv2
-import win32api
-import win32con
-import process
-
-import MyUtils
-
 
 def main(start, *a, **b):
     path = './知乎'
 
 
 if __name__ == "__main__":
-    pass
+    lis=[1,2]
+    print(lis[:4])
